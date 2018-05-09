363c918778780165b381fc0e3d6abcf529565f73
mustbeart <paul@mustbeart.com>
2018-05-08 23:53:12 -0700

    WIP: Implement structure of Mastermind codebreaker UI
    New concept: each Mastermind game is played in both directions at once.
    One player chooses Mastermind from the Games menu, and gets to choose
    their opponent (either a remote badge or the built-in automatic player).
    In case of a remote badge, the other player gets invited to play via a
    pop-up. Each player starts by creating a secret code for the other
    player to guess. Both players then start guessing. The guesses are sent
    over to the opposite badge, where the codemaker's function of providing
    answers is handled automatically with no user intervention.
    
    The user interface is mostly graphical. Pegs are displayed as colored
    boxes on the screen. User entry isn't implemented as of this commit,
    but it will work like this: a moveable arrow is displayed below the
    row of boxes where the entry is to be used. Beside the row of boxes,
    two other menu items appear: Go and Quit. Button function:
    
      UP        change the box above the arrow to the next color
      DOWN      change the box above the arrow to the previous color
      LEFT      move the arrow to the left (NOT cancel as usual on the badge)
      RIGHT     move the arrow to the right
      ACTION    select the menu item above the arrow (if enabled)
    
    The UI starts out with nice big boxes on the screen, but there's only
    room for six rows at that size. On the seventh turn, the boxes rescale
    to make room for twelve rows, which is the standard turn limit for
    Mastermind.
    
    There's a cute little animation of a dialup modem to fill the time
    while a remote connection is being processed.
    
    Lots of this is still stubbed out. Look for "//!!!" flagging places
    that need implementing.

--- firmware/src/mastermind.c
+++ firmware/src/mastermind.c
@@ -21,9 +21,15 @@
 
 #define MM_NUM_COLUMNS	4
 #define	MM_NUM_COLORS	6
+<<<<<<< 251ab9bf26d0113cc440f96623a5956f8681e99e
 #define MM_NO_COLOR		(MM_NUM_COLORS)
 #define MM_ANSWER_MATCH	(MM_NUM_COLORS+1)
 #define MM_ANSWER_WHITE	(MM_NUM_COLORS+2)	// right color, wrong column
+=======
+#define MM_NO_COLOR		(MM_NUM_COLORS+1)
+#define MM_ANSWER_MATCH	(MM_NUM_COLORS+2)
+#define MM_ANSWER_WHITE	(MM_NUM_COLORS+3)	// right color, wrong column
+>>>>>>> WIP: Implement structure of Mastermind codebreaker UI
 #define MM_MAX_TURNS	12
 
 #define MM_PLAYING		0
@@ -43,18 +49,24 @@
 #define MM_PEG_WIDTH	((GFX_WIDTH - 2*MM_LEFT_MARGIN - MM_GUTTER)/(2*MM_NUM_COLUMNS)-MM_PEG_KERNING)
 #define MM_GO_X_POSITION		60
 #define MM_FONT_HEIGHT_TO_BASELINE	9
+<<<<<<< 251ab9bf26d0113cc440f96623a5956f8681e99e
 #define MM_ARROW_HEIGHT			8
 #define MM_ARROW_HALF_WIDTH		4
+=======
+>>>>>>> WIP: Implement structure of Mastermind codebreaker UI
 
 #define MM_BACKGROUND_COLOR		0x2081	// very very yellow
 #define MM_DIMMED_COLOR			0x4208	// very dark grey
 #define MM_ENABLED_COLOR		COLOR_LIGHTGREY
 
+<<<<<<< 251ab9bf26d0113cc440f96623a5956f8681e99e
 #define MM_REMOTE			17
 #define MM_AUTO_OPPONENT	18
 
 #define MM_ANIMATION_MS		100
 APP_TIMER_DEF(m_mm_animation_timer);
+=======
+>>>>>>> WIP: Implement structure of Mastermind codebreaker UI
 
 // Structure that holds a single row of color choices,
 // which may be guesses or answers.
@@ -83,6 +95,7 @@ typedef struct {
 // This is the game in which the local user is Codebreaker.
 static mm_game_t game;
 
+<<<<<<< 251ab9bf26d0113cc440f96623a5956f8681e99e
 
 // Structure that holds the information about our worthy opponent.
 typedef struct {
@@ -107,6 +120,16 @@ static mm_color_list_t		m_received_answer;
 // These are the graphical colors used to display the various color indexes.
 const uint16_t mm_colors[] = {
 	COLOR_YELLOW, COLOR_RED, COLOR_MAGENTA, COLOR_BLUE, COLOR_GREEN, COLOR_WHITE,
+=======
+// Identity of our worthy opponent.
+//!!! right now this is text; soon it will need to contain either a
+//!!! Bluetooth address or a flag that play is to proceed locally.
+static ble_badge_list_menu_text_t m_opponent;
+
+// These are the graphical colors used to display the various color indexes.
+const uint16_t mm_colors[] = {
+	COLOR_YELLOW, COLOR_RED, COLOR_ORANGE, COLOR_BLUE, COLOR_GREEN, COLOR_WHITE,
+>>>>>>> WIP: Implement structure of Mastermind codebreaker UI
 	MM_BACKGROUND_COLOR,		// MM_NO_COLOR
 	COLOR_PINK, COLOR_WHITE		// Colors for answers
 };
@@ -115,6 +138,46 @@ const uint16_t mm_colors[] = {
 // rows need to be displayed.
 static uint8_t m_row_height;
 
+<<<<<<< 251ab9bf26d0113cc440f96623a5956f8681e99e
+=======
+// Update a single row of the graphical display with color boxes
+// for all of the guesses. This is used when redrawing the screen
+// after a change in m_row_height, but not when actually entering guesses.
+static void __draw_guesses(uint8_t row) {
+	uint8_t y = SUBMENU_TITLE_SIZE + 1 + row * m_row_height + 2;
+	uint8_t x = MM_LEFT_MARGIN;
+
+	for (uint8_t peg = 0; peg < MM_NUM_COLUMNS; peg++) {
+		uint16_t color = mm_colors[game.turn[row].guess.peg[peg]];
+		util_gfx_fill_rect(x, y, MM_PEG_WIDTH, m_row_height - MM_LEADING, color);
+		x += MM_PEG_WIDTH + MM_PEG_KERNING;
+	}
+}
+
+// Update a single row of the graphical display with color boxes
+// for all of the answers. This is used both when the answers first
+// arrive and when redrawing the screen after a change in m_row_height.
+static void __draw_answers(uint8_t row) {
+	uint8_t y = SUBMENU_TITLE_SIZE + 1 + row * m_row_height + 2;
+	uint8_t x = 69;
+
+	for (uint8_t peg = 0; peg < MM_NUM_COLUMNS; peg++) {
+		uint16_t color = mm_colors[game.turn[row].answer.peg[peg]];
+		util_gfx_fill_rect(x, y, MM_PEG_WIDTH, m_row_height - MM_LEADING, color);
+		x += MM_PEG_WIDTH + MM_PEG_KERNING;
+	}
+}
+
+// Draw a single blank color box, consisting of an outline in dark grey
+// around a box filled with black. Used to initialize the guesses, and
+// also used when the user chooses to enter MM_NO_COLOR temporarily.
+static void __draw_peg_outline(uint8_t y, uint8_t peg) {
+	uint8_t x = MM_LEFT_MARGIN + peg * (MM_PEG_WIDTH + MM_PEG_KERNING);
+
+	util_gfx_fill_rect(x, y, MM_PEG_WIDTH, m_row_height - MM_LEADING, COLOR_BLACK);
+	util_gfx_draw_rect(x, y, MM_PEG_WIDTH, m_row_height - MM_LEADING, COLOR_DARKGREY);
+}
+>>>>>>> WIP: Implement structure of Mastermind codebreaker UI
 
 // Current coordinates for the "Go" menu item. This item is updated on
 // the fly (to show whether all guesses have been entered, making it valid
@@ -127,6 +190,7 @@ static void __set_go_xy(uint8_t x, uint8_t y) {
 	m_go_y = y;
 }
 
+<<<<<<< 251ab9bf26d0113cc440f96623a5956f8681e99e
 
 // Display the "Go" menu item in a specified color.
 static void __display_go(uint16_t color) {
@@ -845,6 +909,192 @@ static void __set_auto_opponent(void *p_data) {
 //			MM_AUTO_OPPONENT if the user chose to play against the badge
 //			MM_USER_QUIT if the user canceled.
 static uint8_t __mbp_mastermind_players() {
+=======
+// Display the "Go" menu item in the disabled state.
+static void __disable_go(void) {
+	util_gfx_set_cursor(m_go_x, m_go_y);
+	util_gfx_set_color(MM_DIMMED_COLOR);
+	util_gfx_print("Go ");
+}
+
+// Display the "Go" menu item in the enabled state.
+static void __enable_go(void) {
+	util_gfx_set_cursor(m_go_x, m_go_y);
+	util_gfx_set_color(MM_ENABLED_COLOR);
+	util_gfx_print("Go ");
+}
+
+// Collect guesses from the local user, in place on the graphic display
+// Returns: true if the user guessed and hit Go
+//			false if the user hit Quit
+static bool __collect_guesses(uint8_t row) {
+	uint8_t y = SUBMENU_TITLE_SIZE		// room for the column headers
+	 			+ 1 					// room for the line under the headers
+				+ row * m_row_height	// room for the boxes above this row
+				+ MM_LEADING;			// room for a bit of space between rows
+
+	// Draw blank boxes where the guesses go
+	for (uint8_t peg = 0; peg < MM_NUM_COLUMNS; peg++) {
+		__draw_peg_outline(y, peg);
+	}
+
+	// Draw other menu choices
+	__set_go_xy(MM_GO_X_POSITION,
+				y + m_row_height - MM_FONT_HEIGHT_TO_BASELINE); // bottom-justify text
+	util_gfx_set_cursor(m_go_x, m_go_y);
+	util_gfx_set_color(MM_ENABLED_COLOR);	// Quit is always an option
+	util_gfx_print("   Quit");	// leaving room for "Go" at the beginning
+	__disable_go();
+
+	//!!! Here's where we display the triangle cursor and handle buttons
+	//!!! until the user selects Go or Quit
+
+	nrf_delay_ms(5000); //!!! demo
+	__enable_go();		//!!! this happens when all four guesses are filled in
+	nrf_delay_ms(5000); //!!!
+
+	// Erase other menu choices
+	util_gfx_fill_rect(MM_GO_X_POSITION,
+						y-1, 							// allow for ascender height
+						GFX_WIDTH - MM_GO_X_POSITION,	// to right screen edge
+						m_row_height+1,					// to the very bottom
+						MM_BACKGROUND_COLOR);
+
+	return true;	//!!! actually need to return correct status here.
+}
+
+
+// Send the guess to the code maker (remote or automatic) and display answers
+// Returns: true if we successfully retrieved answers
+//			false if not
+static bool __collect_answers(uint8_t row) {
+
+	//!!! TODO
+
+	return true;
+}
+
+
+// Examine the answers and decide if this was a winning guess.
+// That is, if all the answers are Matches.
+// Returns: true if we got all Matches
+//			false if not
+static bool __check_for_victory(uint8_t row) {
+	bool victory = true;
+
+	for (uint8_t peg=0; peg < MM_NUM_COLUMNS; peg++) {
+		victory &= (MM_ANSWER_MATCH == game.turn[row].answer.peg[peg]);
+	}
+
+	return victory;
+}
+
+
+// Implements the game of Mastermind for the local code breaker.
+static void __mm_codebreaker(void) {
+	uint8_t turn = 0;
+	uint8_t status = MM_PLAYING;
+
+	m_row_height = 16;	// nice big boxes to start with (height in pixels)
+
+	// Initialize the game.
+	game.won = false;
+	game.lost = false;
+	for (uint8_t row = 0; row < MM_MAX_TURNS; row++) {
+		game.turn[row].played = false;
+		game.turn[row].answered = false;
+		for (uint8_t column = 0; column < MM_NUM_COLUMNS; column++) {
+			game.turn[row].guess.peg[column] = MM_NO_COLOR;
+			game.turn[row].answer.peg[column] = MM_NO_COLOR;
+		}
+	}
+
+	// Initial screen draw.
+	util_gfx_fill_screen(MM_BACKGROUND_COLOR);
+
+	util_gfx_set_font(FONT_SMALL);
+	util_gfx_set_cursor(0, 5);
+	util_gfx_set_color(COLOR_GREEN);
+	util_gfx_print("   GUESS    ANSWER");
+
+	util_gfx_draw_line(0, SUBMENU_TITLE_SIZE, GFX_WIDTH, SUBMENU_TITLE_SIZE, SUBMENU_TITLE_FG);
+
+	while (1) {
+		uint8_t row;
+
+		if (turn == 6) {	// time to squeeze the display vertically
+			m_row_height = 9;
+			util_gfx_fill_rect(0,						// left screen edge
+							   SUBMENU_TITLE_SIZE+1,	// just below the line
+							   GFX_WIDTH, GFX_HEIGHT,	// to bottom right corner
+							   MM_BACKGROUND_COLOR);
+			for (row = 0; row < 6; row++) {	// redraw the old rows at new height
+				__draw_guesses(row);
+				__draw_answers(row);
+			}
+		}
+
+		if ( ! __collect_guesses(turn)) {
+			status = MM_USER_QUIT;
+			break;
+		}
+
+		if ( ! __collect_answers(turn)) {
+			status = MM_REMOTE_QUIT;
+			break;
+		}
+
+		__draw_answers(row);
+
+		if (__check_for_victory(row)) {
+			status = MM_VICTORY;
+			break;
+		}
+
+		// Next turn
+		turn++;
+		if (turn >= MM_MAX_TURNS) {	// Oops, used up the turn limit!
+			status = MM_DEFEAT;
+			nrf_delay_ms(2000);		// time to ponder your failure
+			break;
+		}
+	}
+
+	char msg_buf[60];
+	switch (status) {
+		case MM_USER_QUIT:
+				sprintf(msg_buf, "You quit the game!");
+				break;
+		case MM_REMOTE_QUIT:
+				sprintf(msg_buf, "Couldn't get an answer from the other player!");
+				break;
+		case MM_VICTORY:
+				sprintf(msg_buf, "You got it in %d turn%c!", turn-1, turn == 0 ? '!' : 's');
+				break;
+		case MM_DEFEAT:
+				sprintf(msg_buf, "You had %d tries but still didn't get it!", MM_MAX_TURNS);
+				break;
+		default:
+				sprintf(msg_buf, "Why is the game over?");
+				break;
+	}
+	mbp_ui_popup("GAMEOVER", msg_buf);
+}
+
+
+// Handle the action of selecting an opponent from the menu.
+static void __set_opponent(void *p_data) {
+	memcpy(m_opponent.text, ((ble_badge_list_menu_text_t *)p_data)->text, 20);
+}
+
+
+// Present a menu of available opponents to choose from.
+//!!! This currently uses the text-only listing from JoCo's badge list.
+//!!! It needs to be updated to use a complete list with Bluetooth addresses.
+// Returns: true if the user picked an opponent.
+//			false if the user canceled.
+static bool __mbp_mastermind_players() {
+>>>>>>> WIP: Implement structure of Mastermind codebreaker UI
 	int badge_list_size;
 	ble_badge_list_menu_text_t *list;
 	uint8_t status;
@@ -929,12 +1179,38 @@ static bool __mm_connect_auto_opponent(mm_opponent_t opponent) {
 }
 
 
+<<<<<<< 251ab9bf26d0113cc440f96623a5956f8681e99e
 static bool __mm_connect_opponent(mm_opponent_t opponent) {
 	if (opponent.remote) {
 		return __mm_connect_remote_opponent(opponent);
 	} else {
 		return __mm_connect_auto_opponent(opponent);
 	}
+=======
+// Make a connection to the remote opponent.
+static bool __mm_connect_opponent(char *text) {
+	util_gfx_cursor_area_reset();
+	mbp_ui_cls();
+
+	//!!! here we will try to make a Bluetooth connection
+	//!!! when it's up, we'll start the codemaker server for incoming guesses
+
+	// Fake dialog to keep the user entertained ...
+	util_gfx_set_font(FONT_SMALL);
+	util_gfx_set_cursor(0, 5);
+	util_gfx_set_color(COLOR_WHITE);
+	util_gfx_print("+++");
+	nrf_delay_ms(800);
+	util_gfx_print("\nOK\nATZ\n");
+	nrf_delay_ms(800);
+	util_gfx_print("OK\nATDT8675309\n");
+	nrf_delay_ms(1700);
+
+	//!!! here we will wait for the connection if it's not already up
+
+	util_gfx_print("CONNECT\n");
+	return true;
+>>>>>>> WIP: Implement structure of Mastermind codebreaker UI
 }
 
 
@@ -944,10 +1220,14 @@ static bool __mm_connect_opponent(mm_opponent_t opponent) {
 // The codemaker function is automatic, except for choosing the code.
 void mastermind() {
 
+<<<<<<< 251ab9bf26d0113cc440f96623a5956f8681e99e
 	// Try to collect the codemaker pegs from the user
 	if (! __mm_make_code()) {
 		return;
 	}
+=======
+	//!!! here we need to collect the codemaker pegs from the user
+>>>>>>> WIP: Implement structure of Mastermind codebreaker UI
 
 	// Try to pick an opponent from the available players
 	if (__mbp_mastermind_players()) {
@@ -960,7 +1240,11 @@ void mastermind() {
 			return;
 		}
 
+<<<<<<< 251ab9bf26d0113cc440f96623a5956f8681e99e
 		if (__mm_connect_opponent(m_opponent)) {
+=======
+		if (__mm_connect_opponent(m_opponent.text)) {
+>>>>>>> WIP: Implement structure of Mastermind codebreaker UI
 			__mm_codebreaker();	// play the game locally.
 
 			//!!! here we will join up with the codemaker server,
@@ -977,5 +1261,9 @@ void mastermind() {
 // Entry point from an incoming Bluetooth connection.
 void mastermind_rsvp(void) {
 
+<<<<<<< 251ab9bf26d0113cc440f96623a5956f8681e99e
 	//!!! TODO
+=======
+	// TODO
+>>>>>>> WIP: Implement structure of Mastermind codebreaker UI
 }
